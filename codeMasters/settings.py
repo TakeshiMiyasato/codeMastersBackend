c4d23392c0ec49180d079a8f590d406dfad16ad7
TakeshiMiyasato <70245318+TakeshiMiyasato@users.noreply.github.com>
2022-07-04 17:09:53 -0400

    Update settings.py

--- codeMasters/settings.py
+++ codeMasters/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vuunnd$qq3h3dzu4b7d-aav#qjd^1a+06_sna3^$2*wifw#gi8'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
